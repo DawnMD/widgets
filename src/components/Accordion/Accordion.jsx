07fa567bd41e9343b9e6ce6dfdba0e74cfc73d34
DawnMD <mainakdas104@gmail.com>
2021-02-17 11:34:10 +0530

    introduced hooks

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ datas }) => {
-  const renderedListItem = datas.map((data) => {
+  const [activeIndex, setActiveIndex] = useState("");
+  const onDataClick = (index) => {
+    setActiveIndex(index);
+  };
+  const renderedListData = datas.map((data, index) => {
     return (
       <React.Fragment key={data.title}>
-        <div className="active title">
+        <div className="active title" onClick={() => onDataClick(index)}>
           <i className="dropdown icon"></i>
           {data.title}
         </div>
@@ -14,7 +18,12 @@ const Accordion = ({ datas }) => {
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedListItem}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedListData}
+      <h1>{`Clicked ${activeIndex} index`}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
